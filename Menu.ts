70a5e2fb4846f29c9bfb6c9533eabd8d16ca02bc
nnandak <kannandakarine@gmail.com>
2025-06-05 10:27:22 -0300

    atualização

--- Menu.ts
+++ Menu.ts
@@ -1,4 +1,5 @@
 import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from "./src/model/ContaPoupanca";
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
 import { Conta } from './src/model/Conta';
@@ -6,11 +7,23 @@ import { Conta } from './src/model/Conta';
 export function main() {
 
     let opcao: number;
+    
+   // Objeto da Classe ContaCorrente (Teste)
+    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
+    contacorrente.visualizar();
+    contacorrente.sacar(2000);
+    contacorrente.visualizar();
+    contacorrente.depositar(1000);
+    contacorrente.visualizar();
 
- //   const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
- //   conta.visualizar();
-   const contaCorrente: ContaCorrente = new ContaCorrente(1, 123, 1, "Adriana", 10000, 5000);
-   contaCorrente.visualizar();
+    // Objeto da Classe ContaPoupanca (teste)
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
+
     while (true) {
 
         console.log(colors.bg.black, colors.fg.yellow, 
@@ -31,8 +44,7 @@ export function main() {
         console.log("            9 - Sair                                 ");
         console.log("                                                     ");
         console.log("*****************************************************");
-        console.log("                                                     ", 
-        colors.reset);
+        console.log("                                                     ", colors.reset);
 
         console.log("Entre com a opção desejada: ");
         opcao = readlinesync.questionInt("");
